e01745a4844ed99d518333ec5f4e6b7da8e811cc
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-07-12 18:54:55 +0530

    Connect React app to get all employees rest api

--- ems-ui/src/components/ListEmployeeComponents.jsx
+++ ems-ui/src/components/ListEmployeeComponents.jsx
@@ -1,49 +1,34 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react';
+import { listEmployees } from '../services/EmployeeService';
 
 const ListEmployeeComponents = () => {
 
-    const dummyData = [
-        {
-            "id":1,
-            "firstName":"Bhawna",
-            "lastName": "Pillai",
-            "email": "bhawna@gmail.com"
-        },
-        {
-            "id":2,
-            "firstName":"Anita",
-            "lastName": "Pillai",
-            "email": "Anita@gmail.com"
-        },
-        {
-            "id":3,
-            "firstName":"Jon",
-            "lastName": "Doe",
-            "email": "jondoe@gmail.com"
-        },
-        {
-            "id":4,
-            "firstName":"Eren",
-            "lastName": "Smith",
-            "email": "eren@gmail.com"
-        }
-    ]
+    const [employees,setEmployees] = useState([])
+    useEffect(()=>{
+        listEmployees().then((response)=>{
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    },[])
 
   return (
     <div className='container'>
+
         <h2 className='text-center'>List of Employees</h2>
         <table className='table table-striped table-bordered'>
             <thead>
                 <tr>
+                    <th>Employee Id</th>
                     <th>Employee First Name</th>
                     <th>Employee Last Name</th>
                     <th>Employee Email Id</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                
                     {
-                        dummyData.map(employee => 
+                        employees.map(employee => 
                             <tr key = {employee.id}>
                                 <td>{employee.id}</td>
                                 <td>{employee.firstName}</td>
@@ -52,7 +37,7 @@ const ListEmployeeComponents = () => {
                             </tr>
                         )
                     }
-                </tr>
+                
             </tbody>
         </table>
     </div>
