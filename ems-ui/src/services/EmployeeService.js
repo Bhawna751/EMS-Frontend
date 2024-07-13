6348bc7424c609d2dfa9081ee8c8fcf745227307
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-07-13 08:19:27 +0530

    Connect React app to Get Employee REST API

--- ems-ui/src/services/EmployeeService.js
+++ ems-ui/src/services/EmployeeService.js
@@ -4,3 +4,4 @@ export const listEmployees= () => {
     return axios.get(REST_API_BASE_URL);
 }
 export const createEmployee = (employee) => axios.post(REST_API_BASE_URL,employee);
+export const getEmployee = (employeeId) => axios.get(REST_API_BASE_URL + '/' + employeeId);
